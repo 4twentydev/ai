f4600417af13c092198e5fffb49a533614806d4e
4twentydev <4twenty.dev@gmail.com>
2025-12-06 17:11:40 -0700

    init

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: 3001,
+  JOB_ROOT: "\\\\SERVER\\Jobs\\",
+  AGENT_ENDPOINT: "http://127.0.0.1:3002/run"
+};
---
+++ backend/jobs/mockJobs.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "UCLA 23017": {
+    R21: {
+      cut: "\\\\SERVER\\Jobs\\UCLA_23017\\R21\\CUT.pdf",
+      packing: "\\\\SERVER\\Jobs\\UCLA_23017\\R21\\PACKING_LIST.pdf",
+      assembly: "\\\\SERVER\\Jobs\\UCLA_23017\\Assembly\\Assembly_R21.pdf"
+    }
+  }
+};
